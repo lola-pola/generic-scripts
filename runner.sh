2e63db17cb9ec20eb0738cbaff0f2d30b1434f50
Elhay Efrat <elhayefrat@gmail.com>
2023-01-18 11:25:39 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -9,5 +9,5 @@ apt install -y dotnet-runtime-3.1
 mkdir myagent && cd myagent && sudo chmod -R 777 *
 wget https://vstsagentpackage.azureedge.net/agent/2.214.1/vsts-agent-linux-x64-2.214.1.tar.gz
 tar zxvf vsts-agent-linux-x64-2.214.1.tar.gz
-export AGENT_ALLOW_RUNASROOT="1" && ./config.sh remove --url https://dev.azure.com/elhayefrat --auth pat --token 
-export AGENT_ALLOW_RUNASROOT="1" && ./config.sh --replace --work _work --acceptTeeEula --url https://dev.azure.com/elhayefrat --auth pat --token '6zyos5pnakiyfb6iqn5kxvjlkpkteadh5ake6hwta2relbyxuaoq' --agent agent-${hostname} --pool 'test' && ./run.sh --once
+export AGENT_ALLOW_RUNASROOT="1" && ./config.sh remove --url https://dev.azure.com/elhayefrat --auth pat --token ${token}
+export AGENT_ALLOW_RUNASROOT="1" && ./config.sh --replace --work _work --acceptTeeEula --url https://dev.azure.com/elhayefrat --auth pat --token ${token} --agent agent-${hostname} --pool 'test' && ./run.sh --once
