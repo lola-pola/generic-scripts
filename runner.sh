812407bcef94f0d91a96d1ac8b1bb46861c15da2
Elhay Efrat <elhayefrat@gmail.com>
2023-01-18 12:32:30 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,13 +1,13 @@
 $1=token
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-apt update 
-apt install -y apt-transport-https 
-apt install -y dotnet-sdk-3.1 
-apt install -y apt-transport-https 
-apt install -y dotnet-runtime-3.1 
-mkdir myagent && cd myagent && sudo chmod -R 777 *
-wget https://vstsagentpackage.azureedge.net/agent/2.214.1/vsts-agent-linux-x64-2.214.1.tar.gz
-tar zxvf vsts-agent-linux-x64-2.214.1.tar.gz
-export AGENT_ALLOW_RUNASROOT="1" && ./config.sh remove --url https://dev.azure.com/elhayefrat --auth pat --token ${token}
-export AGENT_ALLOW_RUNASROOT="1" && ./config.sh --replace --work _work --acceptTeeEula --url https://dev.azure.com/elhayefrat --auth pat --token ${token} --agent agent-${hostname} --pool 'test' && ./run.sh --once
+sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update 
+sudo apt install -y apt-transport-https 
+sudo apt install -y dotnet-sdk-3.1 
+sudo apt install -y apt-transport-https 
+sudo apt install -y dotnet-runtime-3.1 
+sudo mkdir myagent && sudo cd myagent && sudo chmod -R 777 *
+sudo wget https://vstsagentpackage.azureedge.net/agent/2.214.1/vsts-agent-linux-x64-2.214.1.tar.gz
+sudo tar zxvf vsts-agent-linux-x64-2.214.1.tar.gz
+export AGENT_ALLOW_RUNASROOT="1" && sudo -E bash -c ./config.sh remove --url https://dev.azure.com/elhayefrat --auth pat --token ${token}
+export AGENT_ALLOW_RUNASROOT="1" && sudo -E bash -c ./config.sh --replace --work _work --acceptTeeEula --url https://dev.azure.com/elhayefrat --auth pat --token ${token} --agent agent-${hostname} --pool 'test' && ./run.sh --once
